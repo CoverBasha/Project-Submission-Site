e891e56aef3bb8691bab4e20cf87238dfc9acf2c
CoverBasha <ahmedgerab7@gmail.com>
2024-12-16 14:27:05 +0200

    Added a bunch

--- Project Submission Site/Controllers/UserHomeController.cs
+++ Project Submission Site/Controllers/UserHomeController.cs
@@ -5,11 +5,34 @@ namespace Project_Submission_Site.Controllers
 {
     public class UserHomeController : Controller
     {
+        ApplicationContext _context;
         User current;
+
+        public UserHomeController(ApplicationContext context)
+        {
+            _context = context;
+        }
         public IActionResult Home(User user)
         {
             current = user;
             return View(current);
+        }
+
+        public IActionResult Projects()
+        {
+            var projects = _context.Projects.Where(p => p.Status == 0).ToList();
+            return View(projects);
+        }
+
+        public IActionResult Profile()
+        {
+            return View(current);
+        }
+
+        public IActionResult Logout()
+        {
+            current = null;
+            return RedirectToAction("Empty", "Login");
         }
     }
 }
