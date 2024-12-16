214f3e5110a2c71b24bc3ef765b1a9ee8c3c18da
Muhab <swe.muhabfawzy@gmail.com>
2024-12-16 14:18:14 +0200

    - Finished login/logout with sessions - Fixed registration mail service - Added registration verification popup page

--- Project Submission Site/Migrations/20241216094228_AddedVerifiedBoolean.Designer.cs
+++ Project Submission Site/Migrations/20241216094228_AddedVerifiedBoolean.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Project_Submission_Site.Models;
 
@@ -11,9 +12,10 @@ using Project_Submission_Site.Models;
 namespace Project_Submission_Site.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20241216094228_AddedVerifiedBoolean")]
+    partial class AddedVerifiedBoolean
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Project Submission Site/Models/Accounts/Account.cs
+++ Project Submission Site/Models/Accounts/Account.cs
@@ -6,6 +6,7 @@
         public string Email { get; set; } 
         public string Username { get; set; }
         public string Password { get; set; }
-
+        public bool Verified { get; set; }
+        public string VerifyCode { get; set; }
     }
 }
