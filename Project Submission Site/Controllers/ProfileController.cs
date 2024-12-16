81d193433183809cdc883929d1c00318cfb9dbdc
Muhab <swe.muhabfawzy@gmail.com>
2024-12-16 17:11:11 +0200

    - Finished user profile

--- Project Submission Site/Controllers/ProfileController.cs
+++ Project Submission Site/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Project_Submission_Site.Models;
 using ServiceProvider.Helpers;
 
@@ -19,15 +20,38 @@ namespace Project_Submission_Site.Controllers
 			if (account == null)
 				return RedirectToAction("Empty", "Login");
 
-
 			return View(account);
 		}
 
 		[HttpPost]
-		public IActionResult Update(Account account, string NewPassword, string ConfirmPassword)
+		public IActionResult Update(Account user, string NewPassword, string ConfirmPassword)
 		{
+			Account? account = SessionHelper.GetSession(this, _context);
+			if (account == null)
+				return RedirectToAction("Empty", "Login");
 
-			return View();
+            if (!string.IsNullOrEmpty(NewPassword) && NewPassword != ConfirmPassword)
+				ModelState.AddModelError("Password", "Your password and confirmation do not match.");
+            else if (ModelState.GetValidationState("Password") == ModelValidationState.Valid && user.Password != account.Password)
+				ModelState.AddModelError("Password", "The password confirmation you entered is incorrect.");
+
+			if (
+				ModelState.GetValidationState("Email") == ModelValidationState.Valid
+				&& ModelState.GetValidationState("Username") == ModelValidationState.Valid
+                && ModelState.GetValidationState("Password") == ModelValidationState.Valid
+                )
+			{
+				account.Email = user.Email;
+				account.Username = user.Username;
+
+				if (!string.IsNullOrEmpty(NewPassword))
+					account.Password = NewPassword;
+
+				_context.SaveChanges();
+
+				return RedirectToAction("Empty");
+			}
+			return View("Empty");
 		}
 	}
 }
