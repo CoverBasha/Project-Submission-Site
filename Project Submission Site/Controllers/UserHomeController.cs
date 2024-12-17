686e66a9b7d3fc8b2d70efb8d899ba9888bcfa9d
CoverBasha <ahmedgerab7@gmail.com>
2024-12-17 10:45:25 +0200

    a5er

--- Project Submission Site/Controllers/UserHomeController.cs
+++ Project Submission Site/Controllers/UserHomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Project_Submission_Site.Models;
 using Project_Submission_Site.ViewModels;
@@ -47,6 +48,54 @@ namespace Project_Submission_Site.Controllers
 			if (userid != null && userid > 0)
 				HttpContext.Session.Remove("UserId");
 			return RedirectToAction("Empty", "Login");
+		}
+
+		public IActionResult Projects()
+        {
+			return View(_context.Projects.Where(x => x.Status == Status.Available).ToList());
+        }
+
+		[HttpGet]
+		public IActionResult ProjectDetail(int id)
+        {
+			return View(_context.Projects.SingleOrDefault(p => p.Id == id));
+        }
+
+		[HttpPost]
+		public IActionResult SubmitProject(int id, string file)
+		{
+			int? userid = HttpContext.Session.GetInt32("UserId");
+			Account? account = null;
+
+			if (userid != null && userid > 0)
+				account = _context.Users.Where(x => x.Id == userid.Value).Include(x => x.Projects).FirstOrDefault();
+
+			if (account == null)
+				return RedirectToAction("Empty", "Login");
+			else if (!account.Verified)
+			{
+				ViewBag.VerifyEmail = account.Email;
+				return View("../Login/Empty");
+			}
+
+			Project? project = null;
+			if (id > 0)
+				project = _context.Projects.FirstOrDefault(x => x.Id == id);
+
+			if (project != null)
+			{
+				Directory.CreateDirectory("Proposals");
+				//System.IO.File.Copy(file, String.Format("Proposals/{0}", System.IO.Path.GetFileName(file)));
+
+				Project project1 = project;
+				project.Status = Status.Pending;
+
+				((User)account).Projects.Add(project);
+				_context.SaveChanges();
+
+				return RedirectToAction("Home");
+			}
+			return View("ProjectDetail", id);
 		}
 	}
 }
